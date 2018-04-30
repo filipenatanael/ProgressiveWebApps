d0964dd8fcf3e95536160cc14e86f07eef663647
Filipe Natanael <filipenatanael@live.com>
2018-04-30 02:46:21 -0300

    Notifications

---
+++ Push Notifications/sw.js
@@ -0,0 +1,13 @@
+// Notification Support
+
+// self.addEventListener('message', (e) => {
+//   // Respond to all clients
+//  self.clients.matchAll().then((clients) => {
+//    clients.forEach((client) => {
+//      // Only respond to sending client
+//      if (e.source.id === client.id) {
+//        client.postMessage("[ Private ] Hello from Service Worker.");
+//      }
+//    });
+//  });
+// })
