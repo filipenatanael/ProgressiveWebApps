a54458c3ae0b8939fe769c02a559b23aaf7e0709
Filipe Natanael <filipenatanael@live.com>
2018-05-02 02:27:00 -0300

    Update Readme.md :books:

--- Push Notifications/push-server.js
+++ Push Notifications/push-server.js
@@ -10,7 +10,7 @@ webpush.setVapidDetails(
 );
 
 const pushSubscription = {
-  endpoint: 'https://fcm.googleapis.com/fcm/send/cldZxQiy4po:APA91bHfAsEnvwrKhdF4E1Ek_KwG7l_xRAxXEFJX1WDsGlMFB9OEONYnh19GBs7I_3RLcp6eemPcJLSvoWLEFzpxmZKJh5A_LdlweLbPXX36UiT4MT_8I8bBoJlPV4Kfxu0P9cn_K2FP',
+  endpoint: 'https://fcm.googleapis.com/fcm/send/fGqxXjvXtfs:APA91bEnZnTWlIPlTdRBPqs_xFWPr3YzSjKpFCwBCC_Yz1NTw_iDi5aeZjOficB6aNeY-sNDbRecR7eo8ZgpbKk9kPc4jv-e9OwroEvK7Z8VXe8TyPpxS4vriv9nNkvxGjOqEVMqHUtv',
   keys: {
     auth: 'EaBtZ4-Y6tscUO_P9QXndQ==',
     p256dh: 'BKCow82Ve2vfQHaUyjiMs1io1_kuw3Z8Hit9I6tl87sJ2r5n8WZ-kE_2LIbHkEsfOSmzi6Qf-5FPkt9TwTLb1Q0='
