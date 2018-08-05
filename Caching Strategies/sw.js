affd94dd9ccb0f1fe2b0daf80ac38cead96a017c
Filipe Natanael <filipenatanael@live.com>
2018-08-04 23:53:06 -0300

    Service Worker fetch handler

--- Caching Strategies/sw.js
+++ Caching Strategies/sw.js
@@ -1,55 +1,50 @@
-// Service Worker
+// Cache name
+const CACHE_NAME = 'pwa-cache-2.5';
+// Assets to cache
+const STATIC_ASSETS = ['./', './main.js', 'index.html', '../Resources/style.css', '../Resources/logo.png'];
 
-const MYCACHE = 'pwa-cache-2.5';
+// Service Worker fetch handler
+self.addEventListener('fetch', (e) => {
+  console.log(e.request);
+  // 3. Network with cache fallback
+  e.respondWith(
+    fetch(e.request).then((res) => {
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, res));
+      return res.clone();
+    }).catch(error => caches.match(e.request))
+  );
+  // 2. Cache with Network Fallback
+  // e.respondWith(
+  //   caches.match(e.request).then(res) => {
+  //     if(res) return res;
+  //     // Fallback
+  //     return fetch(e.request).then((newRes) => {
+  //       caches.open(CACHE_NAME).then(cache => put.request, newRes);
+  //       return newRes.clone();
+  //     })
+  //   }
+  // );
 
-self.addEventListener('install', (e) => {
-
-  let cacheReady = caches.open(MYCACHE).then((cache) => {
-    console.log('New cache ready!');
-    return cache.addAll([
-      './',
-      './main.js',
-      '../Resources/style.css',
-      '../Resources/logo.png'
-    ]);
-  });
-
-  e.waitUntil(cacheReady);
+  /* 1. Cache only. Static assets - App Shell
+  e.respondWith(caches.match(e.request)); */
 });
 
 
+// Service Worker Install
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => { return cache.addAll(STATIC_ASSETS) })
+  );
+});
+
+// Cache Cleanup
 self.addEventListener('activate', (e) => {
   let cacheCleaned = caches.keys().then((keys) => {
     keys.forEach((key) => {
-      if (key !== MYCACHE) {
+      if (key !== CACHE_NAME) {
         return caches.delete(key);
       }
     });
   });
   e.waitUntil(cacheCleaned);
-});
-
-
-self.addEventListener('fetch', (e) => {
-  // Skip for remote fetch
-  if (!e.request.url.match(location.origin)) return;
-
-  // Serve local fetch from cache
-  let newResponse = caches.open(MYCACHE).then((cache) => {
-    return cache.match(e.request).then((response) => {
-      // Check request was found in cache, if true return them!
-      if (response) {
-        console.log(`Serving ${response.url} from cache.`);
-        return response;
-      }
-
-      return fetch(e.request).then((fetchResponse) => {
-        cache.put(e.request, fetchResponse.clone());
-        return fetchResponse;
-      });
-
-    });
-  });
-
-  e.respondWith(newResponse);
 });
--- Storage Options/sw.js
+++ Storage Options/sw.js
@@ -1,28 +1,55 @@
-/* Service Worker */
+// Service Worker
 
-/* Cache name */
-const CACHE_NAME = 'pwa-cache-2.5';
+const MYCACHE = 'pwa-cache-2.5';
 
-/* Assets to cache */
-const STATIC_ASSETS = ['/', 'index.html', '../Resources/style.css', '/main.js', '../Resources/logo.png'];
-
-
-
-/* Service Worker Install */
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
-  );
+
+  let cacheReady = caches.open(MYCACHE).then((cache) => {
+    console.log('New cache ready!');
+    return cache.addAll([
+      './',
+      './main.js',
+      '../Resources/style.css',
+      '../Resources/logo.png'
+    ]);
+  });
+
+  e.waitUntil(cacheReady);
 });
 
-/* Cache Cleanup */
+
 self.addEventListener('activate', (e) => {
   let cacheCleaned = caches.keys().then((keys) => {
     keys.forEach((key) => {
-      if (key !== PWA_CACHE) {
+      if (key !== MYCACHE) {
         return caches.delete(key);
       }
     });
   });
   e.waitUntil(cacheCleaned);
+});
+
+
+self.addEventListener('fetch', (e) => {
+  // Skip for remote fetch
+  if (!e.request.url.match(location.origin)) return;
+
+  // Serve local fetch from cache
+  let newResponse = caches.open(MYCACHE).then((cache) => {
+    return cache.match(e.request).then((response) => {
+      // Check request was found in cache, if true return them!
+      if (response) {
+        console.log(`Serving ${response.url} from cache.`);
+        return response;
+      }
+
+      return fetch(e.request).then((fetchResponse) => {
+        cache.put(e.request, fetchResponse.clone());
+        return fetchResponse;
+      });
+
+    });
+  });
+
+  e.respondWith(newResponse);
 });
