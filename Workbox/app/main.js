5e12ea11763d64d213be1c182a0f467eaf25b9d1
Filipe Natanael <filipenatanael@live.com>
2018-09-08 17:11:18 -0300

    Install Toastr

---
+++ Workbox/app/main.js
@@ -0,0 +1,39 @@
+const _init = () => {
+
+  // Switch on camera in viewfinder
+  $('#viewfinder').on("show.bs.modal", () => {
+    console.log('camera on');
+  });
+
+  // Switch off camera in viewfinders
+  $('#viewfinder').on("hidden.bs.modal", () => {
+    console.log('camera off');
+  });
+
+  // Take photo
+  $('#shutter').on("click", () => {
+    console.log('take photo');
+  });
+
+  // Submit messages
+  $('#send').on("click", () => {
+
+    // Get caption text
+    let caption = $('#caption').val();
+
+    // Check message is ok
+    if ( !caption ) {
+
+      // Show notification and return
+      toastr.warning('Photo & Caption Required.', 'Incomplete Message');
+      return;
+    }
+
+    console.log('adding message');
+    console.log(caption);
+
+    // Reset caption & photo on success
+    $('#caption').val('');
+
+  });
+};
