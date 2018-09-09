97f6bd3136e19afb72e3063256c826b005cfac70
Filipe Natanael <filipenatanael@live.com>
2018-09-08 22:43:46 -0300

    Show photo preview in camera buttom

--- Workbox/app/main.js
+++ Workbox/app/main.js
@@ -3,30 +3,28 @@ const camera = new Camera( $('#player')[0] );
 
 const _init = () => {
 
-  // Switch on camera in viewfinder
   $('#viewfinder').on("show.bs.modal", () => {
     camera.switch_on();
   });
 
-  // Switch off camera in viewfinders
   $('#viewfinder').on("hidden.bs.modal", () => {
     camera.switch_off();
   });
 
   // Take photo
   $('#shutter').on("click", () => {
-    console.log('take photo');
+    let photo = camera.take_photo()
+
+    // Show photo preview in camera buttom
+    $('#camera').css('background-image', `url(${ photo })`)
+
   });
 
   // Submit messages
   $('#send').on("click", () => {
-
-    // Get caption text
     let caption = $('#caption').val();
-
-    // Check message is ok
+    // Check message is OK
     if (!caption) {
-
       // Show notification and return
       toastr.warning('Photo & Caption Required.', 'Incomplete Message');
       return;
